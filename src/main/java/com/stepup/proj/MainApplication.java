eb152ae059f0b55ab5cd3ebf57af1013a681c45c
Mihail Parakevich <bizaro666@mail.ru>
2025-10-21 23:30:40 +0300

    add new method

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,11 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello my friend");
+        System.out.println("Hello friend");
     }
+    
+    public static void doSomething() {
+        System.out.println(1);
+    }
+    
 }
