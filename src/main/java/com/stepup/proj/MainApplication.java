340adf71c896b88e0e1fc5de9700080e0fe3499e
Mihail Parakevich <bizaro666@mail.ru>
2025-10-25 18:43:20 +0300

    добавлен вызов doSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,6 +5,7 @@ public class MainApplication {
         System.out.println("------------");
         System.out.println("Hello world!");
         System.out.println("Hello friend");
+        doSomething();
         System.out.println("------------");
     }
     
