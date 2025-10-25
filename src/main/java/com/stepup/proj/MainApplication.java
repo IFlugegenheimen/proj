38c3b86fe6dfe1b3298d246603ac275ff825dcb7
Mihail Parakevich <bizaro666@mail.ru>
2025-10-25 18:12:09 +0300

    update logic doSomething method

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -8,8 +8,8 @@ public class MainApplication {
     
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
     }
     
 }
